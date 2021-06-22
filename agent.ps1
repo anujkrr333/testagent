c097b73bbd820cebe85ca3753f8c27805b19c365
anujkrr333 <86239977+anujkrr333@users.noreply.github.com>
2021-06-22 13:53:57 +0530

    Update agent.ps1

--- agent.ps1
+++ agent.ps1
@@ -1,1 +1,1 @@
-wget "https://api.github.com/repos/anujkrr333/testagent/contents/agent.ps1?access_token=asdf" -outfile C:\Users\akumar24\Desktop\poc.ps1
+wget "https://api.github.com/repos/anujkrr333/testagent/contents/agent.ps1?access_token=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" -outfile C:\Users\akumar24\Desktop\poc.ps1
